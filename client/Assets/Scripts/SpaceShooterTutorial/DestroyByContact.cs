4ac909efc90ef22c5365e7d5b867bc40a3fb2392
Deepti Naidu <Deepti.Naidu@marklogic.com>
2015-09-24 20:02:09 -0400

    committing before an update

---
+++ client/Assets/Scripts/SpaceShooterTutorial/DestroyByContact.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.CompareTag("Boundary") || other.CompareTag("Enemy") 
+		         || other.CompareTag("Powerup1") || other.CompareTag("Powerup2"))
+		{
+			Debug.Log ("this is the return function: " + other.tag);
+			return;
+		}
+
+		if (explosion != null)
+		{
+			Instantiate(explosion, transform.position, transform.rotation);
+		}
+
+		gameController.AddScore(scoreValue);
+//		if (other.CompareTag("Undefined")) {
+//			Debug.Log("undefined is this: " + other.tag);
+//		}
+		if (other.CompareTag("Player"))
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver();
+		}
+
+		if (gameObject.name.StartsWith("Enemy Ship"))
+		{
+			Debug.Log ("why is it coming here : " + gameObject.tag);
+			gameController.OnEnemyKilled();
+		}
+		else if (gameObject.name.StartsWith ("Asteroid"))
+		{
+			gameController.OnAsteroidDestroyed();
+		}
+
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
